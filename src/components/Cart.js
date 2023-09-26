b0fcc894dfac02d065300a6d28092293e6036bc5
Hyppolite <roinujtak@gmail.com>
2023-09-26 18:29:49 +0100

    Edit the Cart component and add a button to empty the cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -8,25 +8,16 @@ function Cart() {
 
 	return isOpen ? (
 		<div className='lmj-cart'>
-			<button
-				className='lmj-cart-toggle-button'
-				onClick={() => setIsOpen(false)}
-			>
-				Fermer
-			</button>
+			<button className='lmj-cart-toggle-button' onClick={() => setIsOpen(false)}>Fermer</button>
 			<h2>Panier</h2>
 			<div>Monstera : {monsteraPrice}€</div>
 			<button onClick={() => updateCart(cart + 1)}>Ajouter</button>
 			<h3>Total : {monsteraPrice * cart}€</h3>
+            <button onClick={() => updateCart(0)}>Vider le panier</button>
 		</div>
 	) : (
 		<div className='lmj-cart-closed'>
-			<button
-				className='lmj-cart-toggle-button'
-				onClick={() => setIsOpen(true)}
-			>
-				Ouvrir le Panier
-			</button>
+			<button className='lmj-cart-toggle-button' onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
 		</div>
 	)
 }

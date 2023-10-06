04f8630e2fc9be3d827975660b6cc2833a130fe3
Hyppolite <roinujtak@gmail.com>
2023-10-06 18:46:32 +0100

    Clean the component from an irrelevant child component and adding comments

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,30 @@ import logo from '../assets/logo.png'
 import Banner from './Banner'
 import Cart from './Cart'
 import ShoppingList from './ShoppingList'
-import QuestionForm from './QuestionForm'
 import Footer from './Footer'
 import '../styles/Layout.css'
 
 function App() {
     const title = 'La maison jungle'
-    const [cart, updateCart] = useState([])
+    const [cart, updateCart] = useState([]) // useState declared to the parent component
 
     return (
         <>
-            <QuestionForm />
             <Banner>
                 <img src={logo} alt='La maison jungle' className='lmj-logo' />
                 <h1 className='lmj-title'>{title}</h1>
             </Banner>
             <div className='lmj-layout-inner'>
-				<Cart cart={cart} updateCart={updateCart} />
-				<ShoppingList cart={cart} updateCart={updateCart} />
+				<Cart 
+                    // props
+                    cart={cart}
+                    updateCart={updateCart}
+                />
+				<ShoppingList 
+                    // props
+                    cart={cart}
+                    updateCart={updateCart}
+                 />
 			</div>
             <Footer />
         </>
